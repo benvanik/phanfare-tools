6bcc2ad9258b6daf3c1b3753b139bea9e2d8a4d4
ben.vanik <ben.vanik@4a8338a0-bf4c-0410-8672-abd3a8f61da3>
2008-05-11 02:07:51 +0000

    Enums instead of strings - may need to add some default handling to GetAttributeEnum to make things more forward compatible Added some comparers for sorting

--- trunk/Phanfare.ExternalAPI/PublicProfile.cs
+++ trunk/Phanfare.ExternalAPI/PublicProfile.cs
@@ -6,12 +6,19 @@ using System.Xml;
 
 namespace Phanfare.ExternalAPI
 {
+	public enum Relation
+	{
+		Self,
+		Friend,
+		Family,
+	}
+
 	public class PublicProfile
 	{
 		public long UserID { get; set; }
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
-		public string Relation { get; set; } // self, friend, family
+		public Relation Relation { get; set; }
 		public bool IsPremium { get; set; }
 		public string ImageURL { get; set; }
 
@@ -21,7 +28,7 @@ namespace Phanfare.ExternalAPI
 			profile.UserID = el.GetAttributeInt64( "uid" );
 			profile.FirstName = el.GetAttribute( "first_name" );
 			profile.LastName = el.GetAttribute( "last_name" );
-			profile.Relation = el.GetAttribute( "relation" );
+			profile.Relation = el.GetAttributeEnum<Relation>( "relation" );
 			profile.IsPremium = el.GetAttributeBoolean( "premium" );
 			profile.ImageURL = el.GetAttribute( "image_url" );
 			return profile;
